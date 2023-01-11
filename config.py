47a86ba98d5540309ca65583d07c881c71ce6124
Yan, Xiping <xiping.yan@intel.com>
2023-01-11 10:09:39 +0800

    update config

--- config.py
+++ config.py
@@ -1,16 +1,25 @@
 import sys
 
 physcpubind="0,2,4,6,8"
-ov_bin_folder="/home/tingqian/openvino/bin/intel64/Release"
-model_base="/home/openvino-local-compile-02/common_data/sk_13sept_75models_22.2_int8/"
 
-accuracy_checker_configs = "/home/openvino-local-compile-02/common_data/accuracy-checker/configs/omz_validation/"
+#ov_bin_folder="/home/tingqian/openvino/bin/intel64/Release"
+#model_base="/home/openvino-local-compile-02/common_data/sk_13sept_75models_22.2_int8/"
+#accuracy_checker_configs = "/home/openvino-local-compile-02/common_data/accuracy-checker/configs/omz_validation/"
+#definitions_file=f"{accuracy_checker_configs}/dataset_definitions.yml"
+#data_source=f"/home/tingqian/odt_nfs_share/omz-validation-datasets/"
+#model_attributes=f"{data_source}/model_attributes"
+#annotations=f"/home/tingqian/task/remove_binary_postops/annotations"
+
+ov_bin_folder="/workpath/openvino/bin/intel64/Release"
+model_base="/root/nfs_share/sk_13sept_75models_22.2_int8/"
+
+accuracy_checker_configs = "/root/program/accuracy-checker/configs/omz_validation/"
 definitions_file=f"{accuracy_checker_configs}/dataset_definitions.yml"
 
-data_source=f"/home/tingqian/odt_nfs_share/omz-validation-datasets/"
+data_source=f"/root/nfs_share_2/omz-validation-datasets/"
 model_attributes=f"{data_source}/model_attributes"
 
-annotations=f"/home/tingqian/task/remove_binary_postops/annotations"
+annotations=f"/workpath/dataset/data_cache/annotations"
 
 # 
 # sudo mount -t nfs 10.67.107.130:/home/share/  ./odt_nfs_share/
--- test.sh
+++ test.sh
@@ -2,13 +2,15 @@ cpus=4,5,6,7
 node=0
 new_log=brg.2.8
 base_log=jit.2
-model_dir=/home/luocheng/models_cache/models_cache/
+#model_dir=/home/luocheng/models_cache/models_cache/
+model_dir=/workpath/models/instance-segmentation-security-0228/fp32/
 bin_dir=`pwd`/../openvino/bin/intel64/Release
 
 export ONEDNN_VERBOSE=0
 # without brg
-#numactl -C $cpus -m $node -- python3 test_all.py $bin_dir $base_log $model_dir -dopt
+python3 test_all.py $bin_dir $base_log $model_dir -dopt
 # with brg
-numactl -C $cpus -m $node -- python3 test_all.py $bin_dir $new_log $model_dir -cpu_experimental=brgconv -dopt
-numactl -C $cpus -m $node -- python3 postprocess.py $new_log.log $base_log.log -0.05 $model_dir $bin_dir
-numactl -C $cpus -m $node -- python3 postprocess.py $base_log.log $new_log.log -0.05 $model_dir $bin_dir check_fast
+
+#numactl -C $cpus -m $node -- python3 test_all.py $bin_dir $new_log $model_dir -cpu_experimental=brgconv -dopt
+#numactl -C $cpus -m $node -- python3 postprocess.py $new_log.log $base_log.log -0.05 $model_dir $bin_dir
+#numactl -C $cpus -m $node -- python3 postprocess.py $base_log.log $new_log.log -0.05 $model_dir $bin_dir check_fast
