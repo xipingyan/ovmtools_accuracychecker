80a72704416198966c04b2750723bac5a1b54d90
Xiping Yan <xiping.yan@intel.com>
2023-01-12 15:31:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -10,22 +10,22 @@ physcpubind="0,2,4,6,8"
 #model_attributes=f"{data_source}/model_attributes"
 #annotations=f"/home/tingqian/task/remove_binary_postops/annotations"
 
-ov_bin_folder="/workpath/openvino/bin/intel64/Release"
-model_base="/root/nfs_share/sk_13sept_75models_22.2_int8/"
+ov_bin_folder="../openvino/bin/intel64/Release"
+model_base="../nfs_share/sk_13sept_75models_22.2_int8/"
 
-accuracy_checker_configs = "/root/program/accuracy-checker/configs/omz_validation/"
+accuracy_checker_configs = "/home/xiping/program/accuracy-checker/configs/omz_validation/"
 definitions_file=f"{accuracy_checker_configs}/dataset_definitions.yml"
 
-data_source=f"/root/nfs_share_2/omz-validation-datasets/"
+data_source=f"../nfs_share_2/omz-validation-datasets/"
 model_attributes=f"{data_source}/model_attributes"
 
-annotations=f"/workpath/dataset/data_cache/annotations"
+annotations=f"../dataset/data_cache/annotations"
 
 # Mount test_data and models folder.
-# sudo mkdir -p /root/nfs_share
-# sudo mkdir -p /root/nfs_share_2
-# sudo mount -t nfs 10.67.108.173:/home/vsi/nfs_share /root/nfs_share
-# sudo mount -t nfs 10.67.107.130:/home/share /root/nfs_share_2
+# sudo mkdir -p /home/xiping/nfs_share
+# sudo mkdir -p /home/xiping/nfs_share_2
+# sudo mount -t nfs 10.67.108.173:/home/vsi/nfs_share /home/xiping/nfs_share
+# sudo mount -t nfs 10.67.107.130:/home/share /home/xiping/nfs_share_2
 # 
 #
 if __name__ == "__main__":
